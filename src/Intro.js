07c743162136efab4b07701ca44ac315f0c5d614
Franck <f.martigne@gmail.com>
2019-03-13 08:03:30 +0100

    commented code

--- src/Intro.js
+++ src/Intro.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import posed from 'react-pose';
 
+// creating a animated div (Box) using react-pose
 const Box = posed.div({
   hidden: { opacity: 0 },
   visible: { opacity: 1, transition: { duration: 5000 } }
@@ -9,9 +10,11 @@ const Box = posed.div({
 
 
 class Intro extends Component {
+  // visualStatus state will be used for Box. Default status is hidden
   state = { visualStatus: 'hidden' };
 
   componentDidMount(){
+    // after 5s, visualStatus state is changed to visible. Box then appears
     setTimeout(() => {
       this.setState({visualStatus: 'visible'})
     }, 500)
